3da78d3223727cd94e13a0c6869920f9d5ea6081
Matt Johnson <matt@packettide.com>
2021-01-13 14:19:27 -0500

    PSR-12

--- low_replace/addon.setup.php
+++ low_replace/addon.setup.php
@@ -10,12 +10,12 @@
  */
 
 return array(
-	'author'         => 'Low',
-	'author_url'     => 'http://gotolow.com/',
-	'docs_url'       => 'http://gotolow.com/addons/low-replace',
-	'name'           => 'Low Replace',
-	'description'    => 'Find and replace text by either simple string replacement or regular expression.',
-	'version'        => '3.0.0',
-	'namespace'      => 'Low\Replace',
-	'settings_exist' => FALSE
+    'author'         => 'Low',
+    'author_url'     => 'http://gotolow.com/',
+    'docs_url'       => 'http://gotolow.com/addons/low-replace',
+    'name'           => 'Low Replace',
+    'description'    => 'Find and replace text by either simple string replacement or regular expression.',
+    'version'        => '3.0.0',
+    'namespace'      => 'Low\Replace',
+    'settings_exist' => false
 );
